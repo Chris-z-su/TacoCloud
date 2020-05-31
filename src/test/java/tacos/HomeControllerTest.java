3594a2ddca747bdc48967457450f5fa4d7671445
Administrator <chris_z_su@163.com>
2020-05-31 19:02:48 +0800

    初始版本

---
+++ src/test/java/tacos/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package tacos;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception{
+        mockMvc.perform(get("/"))   //发起对"/"的GET请求
+                .andExpect(status().isOk()) //期望得到HTTP200
+                .andExpect(view().name("home")) //期望的home视图
+                .andExpect(content().string(containsString("Welcome to...")));  //期望包含"Welcome to..."
+    }
+}
