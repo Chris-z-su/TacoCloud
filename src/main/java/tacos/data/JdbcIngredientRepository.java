997a4766040852511de179f45af43fca4c58fc4b
Administrator <chris_z_su@163.com>
2020-06-30 00:24:22 +0800

    使用JDBC读取和写入数据

---
+++ src/main/java/tacos/data/JdbcIngredientRepository.java
@@ -0,0 +1,58 @@
+package tacos.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import tacos.Ingredient;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+//构造器注解，
+//  为JdbcIngredientRepository添加@Repository注解之后，
+//  spring的组件扫描就会自动发现他，
+//  并且会将其初始化为Spring应用上下文中的bean
+@Repository
+public class JdbcIngredientRepository implements IngredientRepository {
+
+    private JdbcTemplate jdbc;
+
+//    当Spring创建JdbcIngredientRepository bean的时候，
+//    它会通过@Autowired标注的构造器将JdbcTemplate注入进来
+    @Autowired
+    public JdbcIngredientRepository(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    @Override
+    public Iterable<Ingredient> findAll() {
+        return jdbc.query(
+                "select id, name, type from Ingredient",
+                this::mapRowToIngredient);
+    }
+
+    @Override
+    public Ingredient findOne(String id) {
+        return jdbc.queryForObject(
+                "select id, name, type from Ingredient where id = ?",
+                this::mapRowToIngredient, id);
+    }
+
+    @Override
+    public Ingredient save(Ingredient ingredient) {
+        jdbc.update(
+                "insert into Ingredient(id, name, type) values(?, ?, ?)",
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getType().toString());
+        return ingredient;
+    }
+
+    private Ingredient mapRowToIngredient(ResultSet rs, int rowNum)
+            throws SQLException {
+        return new Ingredient(
+                rs.getString("id"),
+                rs.getString("name"),
+                Ingredient.Type.valueOf(rs.getString("type")));
+    }
+}
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -2,32 +2,52 @@ package tacos.web;
 
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
+import tacos.Order;
 import tacos.Taco;
+import tacos.data.IngredientRepository;
+import tacos.data.TacoRepository;
 
 import javax.validation.Valid;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
+
+//    注入IngredientRepository对象
+    private final IngredientRepository ingredientRepository;
+
+    private TacoRepository designRepo;
+
+//    @Autowired
+//    public DesignTacoController(IngredientRepository ingredientRepository) {
+//        this.ingredientRepository = ingredientRepository;
+//    }
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository, TacoRepository designRepo) {
+        this.ingredientRepository = ingredientRepository;
+        this.designRepo = designRepo;
+    }
 
     @GetMapping
     public String showDesignForm(Model model){
+        /*
+
         List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Type.WARP),
-                new Ingredient("COTO", "Corn Tortilla", Type.WARP),
+                new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
+                new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
                 new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
                 new Ingredient("CARN", "Carnitas", Type.PROTEIN),
                 new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
@@ -38,25 +58,49 @@ public class DesignTacoController {
                 new Ingredient("SRCR", "Sour Cream", Type.SAUCE)
         );
 
+        */
+
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(
+                i -> ingredients.add(i)
+        );
+
         Type[] types = Ingredient.Type.values();
         for (Type type :
                 types) {
-            model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
         }
 
+        log.info("types info:" + types);
         model.addAttribute("design", new Taco());
 
         return "design";
     }
 
+    @ModelAttribute(name = "order")
+    public Order order(){
+        return new Order();
+    }
+
+    @ModelAttribute(name = "taco")
+    public Taco taco(){
+        return new Taco();
+    }
+
     @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors) {
+    public String processDesign(@Valid Taco design, Errors errors,
+                                @ModelAttribute Order order) {
         if (errors.hasErrors()) {
             return "design";
         }
 
         // Save the taco design...
         // We'll do this in chapter 3
+
+        Taco saved = designRepo.save(design);
+        order.addDesign(saved);
+
         log.info("Processing design: " + design);
         return "redirect:/orders/current";
     }
